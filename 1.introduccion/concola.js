27ce74f2f1aa0da76090856ea34f2f6906070aff
axelyadieldiaz <alexddiax@gamil.com>
2024-09-20 11:06:35 -0500

    subiendo valores operadores
    clase de datos primitivos

---
+++ 1.introduccion/concola.js
@@ -0,0 +1,30 @@
+// para mostrar texto sin formato o texto plano
+console.log("hola mundo")
+// mostarr un mensaje de estilo imformativo
+console.log("mensaje informativo")
+//mostrar un mensaje de estilo a tipo alerta
+console.warn("mensaje de tipo alerta")
+// mostrar un mensaje de estilo 9o tipo error
+console.error("mensaje de tipo error")
+
+// mensaje por consola por gestion de tiempo
+console,time("tiempo de ejecucion")
+edad=18
+if(edad>=18){
+    console.log("eres mayor de edad")
+}else{
+    console.warn("eres ´pulpin")
+}
+
+console.timeEnd("tiempo de ejecucion")
+
+//mensaje de tipo tabla
+console.table("soy una tabla")
+
+lengujes=[
+    {nombre:"javascript", extencion:".js"},
+    {nombre:"python", extencion:".py"},
+    {nombre:"php", extencion:".php"},
+]
+console.log(leguajes)
+console.table(leguajes)
