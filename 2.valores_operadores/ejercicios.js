607cd6261e909cae1e827f3af9ed843b5a9cf527
axelyadieldiaz <alexddiax@gamil.com>
2024-10-02 11:53:47 -0500

    fix:actualizacion de aritmetica
    se agrego los operadores de incremento y decremento

--- 2.valores_operadores/ejercicios.js
+++ 2.valores_operadores/ejercicios.js
@@ -15,3 +15,12 @@ console.log((6+(3/(6+3+(3/2))))/((6+(3/2))/(6+2+6)))
 console.log((6+3+2)/((6+(3/2))/3))
 console.log((6+3+(2/(4*6)))/(6+3+(2/4)))
 console.log((6+(3/(2+8)))/6)
+
+//crear una variable de numero y mostrar por terminal el numero sumado mas uno
+numero=10
+console.log(++numero)
+
+//crear una variable que almacena un numero y que or terminal me muestre el numero original y luego el numero sumado mas uno.
+num=12
+console.log(num++)
+console.log(num)
