698634c0ac5c280a2f09ae68eb45f98e23f2a508
axelyadieldiaz <alexddiax@gamil.com>
2024-12-04 13:10:34 -0500

    feet:haciendo el trabajo de boton

--- 6. DOM/app_tareas/script.js
+++ 6. DOM/app_tareas/script.js
@@ -3,8 +3,13 @@ let button=document.querySelector("#btn-agregar")
 let ul=document.querySelector("#lista-tareas")
 
 button.addEventListener("click",()=>{
-    let li=document.createElement("li")
-    li.textContent=input.value
-    ul.appendChild(li)
+    // let li=document.createElement("li")
+    // li.textContent=input.value
+    // ul.appendChild(li)
+    ul.innerHTML=`
+        <li>${input.value}<button id="delete">eliminar</button></li>
+    `
     input.value=""
 })
+let eliminar=document.querySelector("#delete")
+eliminar.addEventListener("click")
